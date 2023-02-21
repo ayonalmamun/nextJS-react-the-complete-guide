d801c75b1132f3cf7acf93baec51b7545bd6a352
Ayon Al Mamun <ayonalmamun@gmail.com>
2023-02-21 22:02:42 +0600

    implemented context in meetupitem. video 32 done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 import "./index.css";
 import App from "./App";
@@ -10,8 +11,10 @@ const root = createRoot(rootElement);
 
 root.render(
   // <StrictMode>
+  <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </FavoritesContextProvider>
   // </StrictMode>
 );
