7a7edbe2df3e9338f3c113c7bd9b9bde9cca599d
Ayon Al Mamun <ayonalmamun@gmail.com>
2023-02-23 00:14:52 +0600

    router push used. 14 done

--- my-app/src/pages/clients/[id]/index.js
+++ my-app/src/pages/clients/[id]/index.js
@@ -1,14 +1,23 @@
 import { useRouter } from "next/router";
 
 export default function ClientsProjectsPage() {
-    const router = useRouter();
+  const router = useRouter();
 
-    console.log(router.pathname);
-    console.log(router.query);
-    return (
-      <>
-        <h1>Projects of a Given Client</h1>
-      </>
-    );
-  }
-  
+  console.log(router.pathname);
+  console.log(router.query);
+
+  const loadProjectHandler = () => {
+    // router.push('/clients/sultan/banani')
+    // router.replace("/clients/sultan/banani"); // we cant go back after navigation
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "sultan", clientprojectid: "banani" },
+    });
+  };
+  return (
+    <>
+      <h1>Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </>
+  );
+}
