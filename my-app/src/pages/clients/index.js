638c9ccf4de4cd5dd7d30bcb589628fcd83ebd6a
Ayon Al Mamun <ayonalmamun@gmail.com>
2023-02-23 00:02:24 +0600

    12 done. navigating dynamic routes

--- my-app/src/pages/clients/index.js
+++ my-app/src/pages/clients/index.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
-    return (
-      <>
-        <h1>Clients Page</h1>
-      </>
-    );
-  }
-  
+  const clients = [
+    { id: "sultan", name: "Sultan's Dine" },
+    { id: "kachchiV", name: "Kachchi Vai" },
+  ];
+  return (
+    <>
+      <h1>Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{`${client.name}`}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
