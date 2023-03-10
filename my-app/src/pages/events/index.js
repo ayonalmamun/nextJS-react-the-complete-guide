daaa64d5678032ce14425515d0ce5c5e06a4ad49
Ayon Al Mamun <ayonalmamun@gmail.com>
2023-03-10 20:34:11 +0600

    *last commit would have been 10 done* worked on all event page. 11 done

--- my-app/src/pages/events/index.js
+++ my-app/src/pages/events/index.js
@@ -1,8 +1,12 @@
+import { getAllEvents } from "@/dummy-data";
+
+import EventList from "@/components/events/event-list";
+
 export default function AllEventsPage() {
-    return (
-      <>
-        <h1>All Events</h1>
-      </>
-    );
-  }
-  
+  const events = getAllEvents();
+  return (
+    <>
+      <EventList items={events} />
+    </>
+  );
+}
